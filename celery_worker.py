9175ecf1b389876f85759ea0fd2abbde01f57954
Sean Bell <sbell@cs.cornell.edu>
2014-12-27 21:22:27 -0500

    added automatic quoting

--- celery_worker.py
+++ celery_worker.py
@@ -1,3 +1,4 @@
+from pipes import quote as q
 from starcluster.clustersetup import ClusterSetup
 from starcluster.logger import log
 from starcluster import threadpool
@@ -46,8 +47,8 @@ class StartCeleryWorker(WorkerSetup):
 
         if git_sync_dir:
             self._sync_cmd = "; ".join([
-                "sudo mount -o remount '%s'" % remount_dir if remount_dir else "echo no remount",
-                "cd %s" % git_sync_dir,
+                "sudo mount -o remount %s" % q(remount_dir) if remount_dir else "echo no remount",
+                "cd %s" % q(git_sync_dir),
                 "git pull",
                 "git submodule init",
                 "git submodule update",
@@ -60,34 +61,34 @@ class StartCeleryWorker(WorkerSetup):
             '--hostname', '"%%h-%s"' % queue,
         ]
         if queue:
-            celery_args += ['-Q', queue]
+            celery_args += ['-Q', q(queue)]
         if app:
-            celery_args += ['--app', app]
+            celery_args += ['--app', q(app)]
         if broker:
-            celery_args += ['--broker', '"%s"' % broker]
+            celery_args += ['--broker', q(broker)]
         if maxtasksperchild:
-            celery_args += ['--maxtasksperchild', maxtasksperchild]
+            celery_args += ['--maxtasksperchild', int(maxtasksperchild)]
         if concurrency:
-            celery_args += ['--concurrency', concurrency]
+            celery_args += ['--concurrency', int(concurrency)]
         if loglevel:
-            celery_args += ['--loglevel', loglevel]
+            celery_args += ['--loglevel', q(loglevel)]
         if heartbeat_interval:
-            celery_args += ['--heartbeat-interval', heartbeat_interval]
+            celery_args += ['--heartbeat-interval', int(heartbeat_interval)]
         if Ofair:
             celery_args += ['-Ofair']
 
         celery_cmd = "; ".join([
-            "export LD_LIBRARY_PATH=\"" + ld_library_path + ":$LD_LIBRARY_PATH\"",
-            "cd %s" % worker_dir,
+            'export LD_LIBRARY_PATH="' + ld_library_path + ':$LD_LIBRARY_PATH"',
+            'cd %s' % q(worker_dir),
             ' '.join(str(x) for x in celery_args),
         ])
 
         tmux_session = "celery-" + queue
         self._start_cmd = "; ".join([
-            "tmux kill-session -t '%s'" % tmux_session,
-            "sudo mount -o remount '%s'" % remount_dir if remount_dir else "echo no remount",
-            "tmux new-session -s '%s' -d '%s'" % (tmux_session, celery_cmd),
-            "tmux set-option -t '%s' history-limit %s" % (tmux_session, tmux_history_limit),
+            "tmux kill-session -t %s" % q(tmux_session),
+            "sudo mount -o remount %s" % q(remount_dir) if remount_dir else "echo no remount",
+            "tmux new-session -s %s -d %s" % (q(tmux_session), q(celery_cmd)),
+            "tmux set-option -t %s history-limit %s" % (q(tmux_session), tmux_history_limit),
         ])
 
     def on_add_node(self, node, nodes, master, user, user_shell, volumes):
